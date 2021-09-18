38cc641f5bc34887ef9215af54babc4f95252374
lovelyunsh <43734328+lovelyunsh@users.noreply.github.com>
2021-09-18 14:20:45 +0900

    Update baseball.js

--- baseball.js
+++ baseball.js
@@ -22,11 +22,8 @@ function is_allowed_number(number)
         numbers[numbers.length] = tmp;
     }
 	console.log(numbers);
-    if (numbers.size != CONFIG_NUM_DIGIT)
+    if (numbers.length != CONFIG_NUM_DIGIT)
     {
-	    console.log(numbers.size);
-	    console.log(CONFIG_NUM_DIGIT);
-	    console.log(3);
         return false;
     }
     if(numbers[0] == numbers[1] && numbers[0] == numbers[2] && numbers[1] == numbers[2]){
@@ -41,15 +38,15 @@ function get_pool()
 {
     var pool = new Array();
     for (var i = 1; i < 10; i++) {
-	for (var j = 1; j < 10; j++) {
-		for (var k = 1; k < 10; k++) {
-			if (i == j && i == k && j == k){
+	    for (var j = 1; j < 10; j++) {
+		    for (var k = 1; k < 10; k++) {
+			    if (i == j && i == k && j == k){
 				continue;
-                    	}
-			pool[pool.length] = String(i * 100 + j*10 + k);
-			}
-		}
-	}
+			    }
+			    pool[pool.length] = String(i * 100 + j*10 + k);
+		    }
+	    }
+    }
     return pool;
 }
 
